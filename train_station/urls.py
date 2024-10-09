7fada479efc37dc63013b31b7d2e9943dd2663a8
Stepan <oleksiukstepann@gmail.com>
2024-10-09 19:51:26 +0300

    create superuser & update urls

--- train_station/urls.py
+++ train_station/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path(),
+    # path(),
 ]
 
 app_name = "station"
--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path(),
+    # path(),
 ]
+
+app_name = "user"
