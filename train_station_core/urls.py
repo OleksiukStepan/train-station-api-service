e3380b0b8ebb1b838dab6955de79c16e33658a9d
Stepan <oleksiukstepann@gmail.com>
2024-10-09 19:34:42 +0300

    add urls.py to train_stations & user, update urls in train_station_core

--- train_station_core/urls.py
+++ train_station_core/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/station/",
+        include("train_station.urls", namespace="station")
+    ),
+    path("api/v1/user/", include("user.urls", namespace="user")),
 ]
