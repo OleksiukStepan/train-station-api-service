d6b30486df3875cc8c595fe383082ae89dd50cd7
Stepan <oleksiukstepann@gmail.com>
2024-10-09 20:21:40 +0300

    add views to routers

--- train_station/urls.py
+++ train_station/urls.py
@@ -1,8 +1,33 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from train_station.views import (
+    StationViewSet,
+    RouteViewSet,
+    OrderViewSet,
+    TrainViewSet,
+    TrainTypeViewSet,
+    CrewViewSet,
+    JourneyViewSet,
+    TicketViewSet,
+)
+
+
+router = routers.DefaultRouter()
+
+router.register("station", StationViewSet)
+router.register("route", RouteViewSet)
+router.register("orders", OrderViewSet)
+router.register("train", TrainViewSet)
+router.register("train-type", TrainTypeViewSet)
+router.register("crew", CrewViewSet)
+router.register("journey", JourneyViewSet)
+router.register("ticket", TicketViewSet)
+
 
 
 urlpatterns = [
-    # path(),
+    path("", include(router.urls)),
 ]
 
 app_name = "station"
