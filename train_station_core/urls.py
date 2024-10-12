71678ad9e546c8913ca6bc088a41af26d72dedb8
Stepan <oleksiukstepann@gmail.com>
2024-10-12 19:50:55 +0300

    update urls in train_station_service

--- train_station_core/urls.py
+++ train_station_core/urls.py
@@ -26,17 +26,16 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/station/", include("train_station.urls", namespace="station")),
-    path("api/v1/user/", include("user.urls", namespace="user")),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Optional UI:
+    path("api/station/", include("train_station.urls", namespace="station")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/v1/doc/swagger/",
+        "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/v1/doc/redoc/",
+        "api/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
