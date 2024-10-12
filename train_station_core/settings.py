083652cb8552691d7e1eb550994fec62bf925b2f
Stepan <oleksiukstepann@gmail.com>
2024-10-12 19:27:43 +0300

    add Simple JWT & update urls for user

--- train_station_core/settings.py
+++ train_station_core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -158,8 +158,11 @@ REST_FRAMEWORK = {
         "anon": "10/minute",
         "user": "30/minute"
     },
+    # JWT
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
-
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Train Station API",
@@ -169,4 +172,10 @@ SPECTACULAR_SETTINGS = {
         "manage trains, crew members, and stations efficiently.",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
